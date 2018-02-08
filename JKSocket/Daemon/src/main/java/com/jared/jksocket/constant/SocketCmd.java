25d08e78a63435973475abad0ea4ac03fbe09122
eastmoon <chenjianneng1117@gmail.com>
2018-02-08 21:51:31 +0800

    [add]add handler && cmd Signed-off-by: eastmoon <chenjianneng1117@gmail.com>

---
+++ JKSocket/Daemon/src/main/java/com/jared/jksocket/constant/SocketCmd.java
@@ -0,0 +1,10 @@
+package com.jared.jksocket.constant;
+
+/**
+ * Created by jared on 2018/2/8.
+ */
+
+public interface SocketCmd {
+    int CMD1 = 1;
+    int CMD2 = 2;
+}
---
+++ JKSocket/Daemon/src/main/java/com/jared/jksocket/constant/SocketHandler.java
@@ -0,0 +1,10 @@
+package com.jared.jksocket.constant;
+
+/**
+ * Created by jared on 2018/2/8.
+ */
+
+public interface SocketHandler {
+    int Test1 = 1;
+    int Test2 = 2;
+}
